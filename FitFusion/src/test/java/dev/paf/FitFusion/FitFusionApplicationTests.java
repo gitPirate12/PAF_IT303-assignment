1cd437922b5053c674945a0bbdd4398df00a6053
gitPirate12 <aneeqshaffy7@gmail.com>
2024-04-09 22:07:49 +0100

    Initial commit

---
+++ FitFusion/src/test/java/dev/paf/FitFusion/FitFusionApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.paf.FitFusion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitFusionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
