a6cfcb4c6394988844babed9559decbece12532c
Heshan <it21341540@my.sliit.lk>
2024-04-13 22:43:47 +0530

    folder error fixed

--- Backend/src/main/java/dev/paf/FitFusion/FitFusionApplication.java
+++ Backend/src/main/java/dev/paf/FitFusion/FitFusionApplication.java
@@ -1,19 +1,19 @@
-package dev.paf.FitFusion;
+// package dev.paf.FitFusion;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+// @SpringBootApplication
 
-public class FitFusionApplication {
+// public class FitFusionApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FitFusionApplication.class, args);}
+// 	public static void main(String[] args) {
+// 		SpringApplication.run(FitFusionApplication.class, args);}
 
 
 	
 			
 	
-	}
+// 	}
 
 
