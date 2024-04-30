763f8f22406b6e359e99b6b26d132bee44790e0d
Heshan <it21341540@my.sliit.lk>
2024-04-30 19:07:44 +0530

    Merge branch 'feature/heshan.s/meal-plan-management' into development

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import ViewPost from './components/SocialMediaPosts/viewPost';
 
 
 import ViewPost from './components/SocialMediaPosts/viewPost';//post
