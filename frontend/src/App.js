ff0f2a7d99fcea582b3a5ad27ca9f4051075652f
gitPirate12 <aneeqshaffy7@gmail.com>
2024-05-04 02:32:38 +0100

    minor frontend nodemodule fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import ViewPost from './components/SocialMediaPosts/viewPost';//post
+//import ViewPost from './component/SocialMediaPosts/viewPost.jsx';//View post
+import ViewPost from './component/SocialMediaPosts/viewPost';
 import Navbar from './component/Navbar/Navbar';//Nav bar
 import MealPlanForm from './component/CreateMealplan/MealPlanForm';//create Mealplan
 import FetchMealPlans from './component/FetchMealPlans/FetchMealPlans';//View mealplan
@@ -19,7 +20,8 @@ function App() {
         <Route path='/createmeal' element={<MealPlanForm />} />
         <Route path='/viewmealplan' element={<FetchMealPlans />} />
         <Route path='/updatemealplan' element={<MealPlanUpdate />} />
-        <Route path='/viewPost' element={<ViewPost />} />
+        {/* <Route path='/viewPost' element={<ViewPost />} /> */}
+        <Route path='/ViewPost' element={<ViewPost />} />
         <Route path='/AddPost' element={<AddPost />} />
         
       </Routes>
