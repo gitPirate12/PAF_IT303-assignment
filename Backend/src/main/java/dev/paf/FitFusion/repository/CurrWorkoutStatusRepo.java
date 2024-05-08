3686ff438b063d02fbed10005ede3dbd6679e881
gitPirate12 <aneeqshaffy7@gmail.com>
2024-05-08 15:34:53 +0100

    fix files

--- Backend/src/main/java/dev/paf/FitFusion/repository/CurrWorkoutStatusRepo.java
+++
@@ -1,9 +0,0 @@
-package dev.paf.FitFusion.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import dev.paf.FitFusion.model.CurrWorkoutStatus;
-
-public interface CurrWorkoutStatusRepo extends MongoRepository<CurrWorkoutStatus,String>{
-
-}
